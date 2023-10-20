fbc28fff1fe2819d59886dadec1890a4484483d7
Fabricio Flores <me@fabricioflores.se>
2023-10-20 15:09:39 +0200

    http method: GET, POST for users

--- src/index.js
+++ src/index.js
@@ -1,20 +1,47 @@
 import express from 'express'
 import { PrismaClient } from "@prisma/client";
-
+import errorHandler from './util/errorHandler.js'
+import AppError from './util/AppError.js'
 
 const app = express();
 const db = new PrismaClient();
 const port = process.env.PORT || 8080;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hi there!");
 })
 
 app.get("/users", async (req, res) => {
-    const users = await db.user.findMany()
+    const users = await db.user.findMany();
     res.json(users)
-})
+});
+
+app.post("/users", async (req, res, next) => {
+    const data = req.body;
+
+    try {
+        if (!data.name || !data.lastname || !data.email) {
+            throw new AppError("Missing fields", 406);
+        }
+
+        const newUser = await db.user.create({
+            data
+        });
+
+        res.json({
+            message: "User created succesfully",
+            user: newUser
+        })
+    } catch (err) {
+        next(err);
+    }
+
+});
+
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
-})
+});
