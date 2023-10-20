545e30f945288274b333fba7f26d0325277e6cfd
Fabricio Flores <me@fabricioflores.se>
2023-10-20 15:09:03 +0200

    custom error handler

---
+++ src/util/errorHandler.js
@@ -0,0 +1,24 @@
+const errorHandler = (err, req, res, next) => {
+    if (err) {
+        console.log(`\nAn error of type ${err.name} ocurred:`);
+        console.log("==============================")
+        console.log(`Trigered by a ${req.method} request at ${req.url}`)
+        console.log(err)
+        console.log("==============================")
+
+        const errorType = err.constructor.name;
+
+        switch (errorType) {
+            case 'AppError':
+                res.status(500).json({ error: err.message });
+                break;
+            case 'PrismaClientValidationError':
+                res.status(406).json({ error: "Validation error, try again." });
+                break;
+            default:
+                res.status(500).json({ error: "Serverside error, try again." });
+        }
+    }
+}
+
+export default errorHandler;
